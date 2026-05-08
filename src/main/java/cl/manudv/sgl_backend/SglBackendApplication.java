ee487cbedefdfc465e1e4fca5e531353a2f6968e
Manuel Alfaro <m.alfaro@live.cl>
2026-05-08 16:14:54 -0400

    create base structure for project

---
+++ src/main/java/cl/manudv/sgl_backend/SglBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.manudv.sgl_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SglBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SglBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/manudv/sgl_backend/SglBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.manudv.sgl_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SglBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
